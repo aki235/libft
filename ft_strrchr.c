7c588b666e57c278daababb6a865e188c9a5ef65
aki235 <104158835+aki235@users.noreply.github.com>
2023-05-25 21:04:54 +0900

    Update ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -2,5 +2,17 @@
 
 char  *ft_strrchr(char *s, int c)
 {
+  int i;
   
+  i = ft_strlen(s) - 1;
+  if (i < 0)
+    return (NULL);
+  while (i >= 0)
+  {
+    if (s[i] == c)
+      return (&s[i]);
+    else
+      i--;
+  }
+  return (NULL);
 }
