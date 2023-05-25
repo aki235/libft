61024a5b36e8c85fa7e451dcead1367c5dad253f
aki235 <104158835+aki235@users.noreply.github.com>
2023-05-25 21:14:10 +0900

    Create ft_strncmp.c

---
+++ ft_strncmp.c
@@ -0,0 +1,16 @@
+#include "header.h"
+
+int strncmp(const char *s1, const char *s2, size_t n)
+{
+  int i;
+  
+  i = 0;
+  while (i < n)
+  {
+    if (s1[i] != s2[i])
+      return (s1[i] - s2[i]);
+    else
+      i++;
+  }
+  return (0);
+}
