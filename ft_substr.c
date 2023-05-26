42d060e1a08cb97b9a6dae1a25cc9f4b9dd71c1a
aki235 <104158835+aki235@users.noreply.github.com>
2023-05-27 04:26:03 +0900

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -2,4 +2,22 @@
 
 char  *ft_substr(char const *s, unsigned int start, size_t len)
 {
+  size_t  i;
+  size_t  j;
+  char    *ret;
   
+  i = 0;
+  j = 0;
+  ret = (char*)malloc(sizeof(*s) * (len + 1));
+  //if (!ret)
+  while (s[i] != '\0')
+  {
+    if (i >= start && j < len)
+    {
+      j++;
+      ret[j] = s[i];
+    }
+    i++;
+  }
+  ret[j] = '\0';
+}
