bbc45931f5205532a0706ef4314ac6fe8d85d9d5
aki235 <104158835+aki235@users.noreply.github.com>
2023-05-26 17:13:50 +0900

    Create ft_memcmp.c

---
+++ ft_memcmp.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+int ft_memcmp(const void *buf1, const void *buf2, size_t, n)
+{
+  size_t i;
+  
+  i = 0;
+  while (i < n)
+  {
+    if (buf1[i] == buf2[i])
+      return (buf1[i] - buf2[i]);
+    i++;
+  }
+  return (0);
+}
