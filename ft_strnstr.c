b45b2e04453fec326a1035981ca56da512402c63
aki235 <104158835+aki235@users.noreply.github.com>
2023-05-26 17:53:25 +0900

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,1 +1,22 @@
+#include "libft.h"
 
+char *ft_strnstr(const char *big, const char *little, size_t len)
+{
+  size_t i;
+  size_t j;
+  
+  if (little == NULL)
+    return (big);
+  i = 0;
+  while (big[i] != '\0')
+  {
+    if (big[i] == little[j] && i < len)
+      j++;
+    else
+      j = 0;
+    i++;
+    if (little[j] == '\0')
+      return (&big[i - j]);
+  }
+  return (NULL);
+}
