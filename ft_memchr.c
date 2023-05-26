16393e9b40a510c232be0dd03109fd067a3ba6a0
aki235 <104158835+aki235@users.noreply.github.com>
2023-05-26 17:10:40 +0900

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -2,15 +2,16 @@
 
 void  *ft_memchr(const void *s, int c, size_t n)
 {
-  int i;
+  size_t i;
   
   i = 0;
-  while (s[i] != 0 && i < n)
+  if (s == NULL)
+    return (NULL);
+  while (i < n)
   {
     if (s[i] == c)
       return (&s[i]);
-    else
-      i++;
+    i++;
   }
   return (NULL);
 }
